2ce5a8876563d65ca0a2d2ec48e37dbcae367bc7
evgenymokhnachev <evgeny.mokhnachev@softeq.com>
2017-10-26 10:57:34 +0300

    remove buffs from Show board

--- dist/game/data/scripts/handlers/communityboard/BuffsBoard.java
+++ dist/game/data/scripts/handlers/communityboard/BuffsBoard.java
@@ -18,14 +18,9 @@
  */
 package handlers.communityboard;
 
-import com.l2jserver.gameserver.data.sql.impl.ClanTable;
 import com.l2jserver.gameserver.handler.CommunityBoardHandler;
 import com.l2jserver.gameserver.handler.IWriteBoardHandler;
-import com.l2jserver.gameserver.model.L2Clan;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jserver.gameserver.network.SystemMessageId;
-import com.l2jserver.gameserver.util.Util;
-import com.l2jserver.util.StringUtil;
 
 /**
  * Buffs board.
